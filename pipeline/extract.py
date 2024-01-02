21859c6e85f7d1497495f13eef8b194f88584116
Jack Hayden <jackhaayden1@gmail.com>
2024-01-02 19:51:03 +0000

    Added extract file

---
+++ pipeline/extract.py
@@ -0,0 +1,38 @@
+"""This script extracts story information from the Hacker News API."""
+
+import pandas as pd
+from requests import get
+
+
+BASE_URL = "https://hacker-news.firebaseio.com/v0/"
+
+
+def get_top_stories() -> list:
+    """Returns the ids of the top 200 stories on Hacker News."""
+    top_stories = get(BASE_URL + "topstories.json", timeout=100).json()
+    return top_stories[:200]
+
+
+def extract_story_info(story_id: int) -> dict:
+    """Finds the details of a given story on Hacker News based on the story id."""
+    story_info = get(BASE_URL + "item/" + str(story_id) + ".json", timeout=100).json()
+    relevant_cols = ["id", "type", "by", "time", "url", "score", "title", "descendants"]
+    story_dict = {col : story_info.get(col) for col in relevant_cols}
+    return story_dict
+
+
+def main() -> None:
+    """Collects information on the 200 top stories and stores it in a csv file."""
+    all_stories = []
+    story_ids = get_top_stories()
+
+    for id in story_ids:
+        all_stories.append(extract_story_info(id))
+
+    stories_df = pd.DataFrame(all_stories)
+    stories_df.to_csv("all_stories.csv")
+
+
+if __name__ == "__main__":
+
+    main()
