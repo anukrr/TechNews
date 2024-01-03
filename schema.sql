ad48197b737b693881e4a5bda9f4cb65cd770eea
Kayode <kayodeapena@gmail.com>
2024-01-03 10:25:27 +0000

    complete first iteration

--- schema.sql
+++ schema.sql
@@ -5,63 +5,28 @@ DROP DATABASE IF EXISTS technews;
 CREATE DATABASE technews;
 \c technews
 DROP TABLE IF EXISTS stories;
-DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS records;
+
 
 CREATE TABLE stories (
-  story_id INT GENERATED ALWAYS AS IDENTITY, 
+  story_id INT, 
   title VARCHAR(250) NOT NULL,
   author VARCHAR(100) NOT NULL,
-  story_url VARCHAR(250) NOT NULL,
-  creation_date TIMESTAMPTZ NOT NULL,
+  story_url VARCHAR(500) NOT NULL,
+  creation_date TIMESTAMP NOT NULL,
   PRIMARY KEY (story_id)
   );
 
 
-CREATE TABLE departments (
-  department_id SMALLINT GENERATED ALWAYS AS IDENTITY, 
-  department_name VARCHAR(50) UNIQUE NOT NULL, 
-  PRIMARY KEY (department_id)
-  );
-
-
-CREATE TABLE exhibitions (
-  exhibit_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-  exhibit_name VARCHAR(50) NOT NULL,
-  exhibit_description VARCHAR(250) NOT NULL,
-  open_date TIMESTAMPTZ NOT NULL,
-  floor_id INT NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (exhibit_id),
-  CONSTRAINT fk_floor 
-    FOREIGN KEY (floor_id) 
-    REFERENCES floors (floor_id)
-    ON DELETE CASCADE,
-  CONSTRAINT fk_department 
-    FOREIGN KEY (department_id) 
-    REFERENCES departments (department_id)
-    ON DELETE CASCADE
-  );
-
-
-CREATE TABLE emergency_events (
-  emergency_id SMALLINT GENERATED ALWAYS AS IDENTITY, 
-  created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
-  exhibit_id SMALLINT NOT NULL,
-  PRIMARY KEY (emergency_id),
-  CONSTRAINT fk_emergency 
-    FOREIGN KEY (exhibit_id) 
-    REFERENCES exhibitions(exhibit_id)
-    ON DELETE CASCADE
-  );
-
-
-CREATE TABLE assistance_events (
-  assistance_id SMALLINT GENERATED ALWAYS AS IDENTITY, 
-  created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
-  exhibit_id SMALLINT NOT NULL,
-  PRIMARY KEY (assistance_id),
-  CONSTRAINT fk_assistance 
-    FOREIGN KEY (exhibit_id) 
-    REFERENCES exhibitions(exhibit_id)
+CREATE TABLE records (
+  record_id INT GENERATED ALWAYS AS IDENTITY, 
+  story_id INT NOT NULL,
+  score SMALLINT NOT NULL,
+  comments INT NOT NULL,
+  record_time TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (record_id),
+  CONSTRAINT fk_story
+    FOREIGN KEY (story_id) 
+    REFERENCES stories(story_id)
     ON DELETE CASCADE
   );
