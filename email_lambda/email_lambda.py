ad208dab5024feca2c3a970b0e10625a9f60d511
anukrr <argkrr@gmail.com>
2024-01-03 17:26:53 +0000

    Minor changes needed to make email prettier

--- email_lambda/email_lambda.py
+++ email_lambda/email_lambda.py
@@ -4,6 +4,8 @@ import pandas as pd
 import boto3
 from datetime import date
 import psycopg2
+from openai import OpenAI
+import json
 
 def get_db_connection():
     '''Forms AWS RDS postgres connection.'''
@@ -19,10 +21,42 @@ def get_db_connection():
     finally:
         conn.close()
 
-def send_email(top_10_articles: list[dict]):
+list_of_url=["https://cachemon.github.io/SIEVE-website/blog/2023/12/17/sieve-is-simpler-than-lru/",
+"https://fixmyblinds.com/",
+"https://www.oreilly.com/library/view/50-algorithms-every/9781803247762/",
+"https://arxiv.org/abs/2312.17661",
+"https://blog.cr.yp.to/20240102-hybrid.html"]
+
+def summarise_story(url_list:list[str]):
+    client = OpenAI(api_key=environ["OPENAI_API_KEY"])
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": "You are a newsletter writer."},
+        {"role": "user", "content": f"Write a comprehensive 150 word summary for articles in this list {url_list}, return a list of dictionaries with keys 'article_title' and 'summary for each article'"}
+        ],
+        temperature=0.5
+    )
+    article_summary = response.choices[0].message.content.strip()
+    return article_summary
+
+
+def get_article_title(url: str):
+    client = OpenAI(api_key=environ["OPENAI_API_KEY"])
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a newsletter writer."},
+            {"role": "user", "content": f"Return title for the article at this {url}"}
+        ],
+        temperature=0.5
+    )
+    title = response.choices[0].message.content.strip()
+    return title
+def send_email(html_string:str):
     today = date.today()
-    date = today.strftime("%B %d")
-    email_html = generate_html_string(top_10_articles)
+    today_date = today.strftime("%B %d")
+    # email_html = generate_html_string(top_10_articles)
     client = boto3.client('ses', region_name='eu-west-2', aws_access_key_id=environ["ACCESS_KEY_ID"],
                           aws_secret_access_key=environ["SECRET_ACCESS_KEY"])
 
@@ -34,12 +68,12 @@ def send_email(top_10_articles: list[dict]):
             'Body': {
                 'Html': {
                     'Charset': 'UTF-8',
-                    'Data': email_html
+                    'Data': html_string
                 }
             },
             'Subject': {
                 'Charset': 'UTF-8',
-                'Data': f'Daily Brief {date}',
+                'Data': f'Daily Brief {today_date}',
             },
         },
         Source='trainee.anurag.kaur@sigmalabs.co.uk'
@@ -47,26 +81,39 @@ def send_email(top_10_articles: list[dict]):
 
     return response
 
-def generate_html_string(plants: list[dict]) -> str:
+def generate_html_string() -> str:
     """Generates HTML string for the email"""
-    today = date.today()
-    warning_string = '<body>'
+    summary = summarise_story(list_of_url)
+    dict_of_summary = json.loads(f"{summary}")
+    html_start = f"""<html>
+    <head>
+    </head>
+    <body>
+    <center class="wrapper">
+        <table class="main" width="700">
+        <tr>
+            <td height="8" style="background-color: #F0F8FF;">
+            </td>
+            </tr>
+    <h1> Daily Brief</h1>
+    <h1 style="color:#5F9EA0">Top Stories</h1>"""
 
-    for plant in plants:
-
-        plant_id = plant.get('plant_id')
-        temperature = plant.get('temperature')
-        avg = plant.get('avg_temp')
-        if temperature > avg:
-            difference = temperature - avg
-            warning_string += f""" <li> Plant {plant_id} is above optimum temperature by {difference}˚C. The average temperature is {avg}˚C but the temperature is {temperature}˚C </li>"""
-
-        if temperature < avg:
-            difference = avg - temperature
-            warning_string += f""" <li> Plant {plant_id} is below optimum temperature by {difference}˚C. The average temperature is {avg}˚C but the temperature is {temperature}˚C</li>"""
-
-    warning_string += '</body>'
-    return warning_string
+    html_end="""</body>
+        </table>
+    </center>
+    </body>
+    </html>"""
+    articles_list = []
+    for article in dict_of_summary:
+        title = article.get('article_title')
+        summary = article.get('summary')
+        article_box = f"""<body style="border-width:3px; border-style:solid; border-color:#E6E6FA; border-radius: 12px; padding: 20px;">
+        <h2 style="color: #008B8B;"> {title}</h2>
+        <p style="color:#6495ED"> {summary} </p> </body>"""
+        articles_list.append(article_box)
+    articles_string = " ".join(articles_list)
+    html_full = html_start + articles_string + html_end
+    return html_full
 
 # def handler(event=None, context=None):
 #     load_dotenv()
@@ -76,3 +123,7 @@ def generate_html_string(plants: list[dict]) -> str:
 #     if unhealthy_plants != []:
 #         return send_email(unhealthy_plants)
 #     return None
+load_dotenv()
+
+html_str=generate_html_string()
+send_email(html_str)
