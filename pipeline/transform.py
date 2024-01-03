770a3295bfaef178ecfd8bb16343dc92604eb485
Jack Hayden <jackhaayden1@gmail.com>
2024-01-03 16:58:52 +0000

    Added topic functionality

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,6 +1,45 @@
 """This script cleans the data collected from the Hacker News API."""
+from os import environ
+from dotenv import load_dotenv
 import pandas as pd
-from datetime import datetime
+
+from openai import OpenAI
+from pandarallel import pandarallel
+
+load_dotenv()
+VALID_TOPICS = ("1","2","3","4","5","6","7","8","9","10","11")
+
+pandarallel.initialize(progress_bar=True)
+
+
+client = OpenAI(
+    api_key = environ["OPENAI_API_KEY"]
+)
+
+
+def generate_topic(story_url: str) -> str:
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system",
+         "content": """You are a classifying bot that can categorise urls into only these categories by returning the corresponding number:
+         1. Programming & Software Development
+         2. Game Development
+         3. Algorithms & Data Structures
+         4. Web Development & Browser Technologies
+         5. Computer Graphics & Image Processing
+         6. Operating Systems & Low-level Programming
+         7. Science & Research Publications
+         8. Literature & Book Reviews
+         9. Artificial Intelligence & Machine Learning
+         10. News & Current Affairs.
+         11. Miscellaneous & Interesting Facts"""},
+        {"role": "user",
+         "content": f"""Categorise this url into one of the listed categories: {story_url}.
+         Only state the category number and nothing else. Ensure your only output is a number."""}
+    ]
+    )
+    return completion.choices[0].message.content
 
 
 def clean_dataframe(stories_df: pd.DataFrame) -> pd.DataFrame:
@@ -12,8 +51,11 @@ def clean_dataframe(stories_df: pd.DataFrame) -> pd.DataFrame:
                                             "url": "story_url"})
     stories_df = stories_df[stories_df.type == "story"]
     stories_df = stories_df.drop(columns="type")
-
+    stories_df["topic_id"] = stories_df["story_url"].parallel_apply(generate_topic)
+    stories_df.loc[~stories_df["topic_id"].isin(VALID_TOPICS), "topic_id"] = None
+    
     return stories_df
+
 
 
 if __name__ == "__main__":
