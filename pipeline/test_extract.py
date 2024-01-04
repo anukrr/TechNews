1a727d0dd0d9d83eeae84a2b47de11f0d4e44e1b
anukrr <argkrr@gmail.com>
2024-01-04 11:18:45 +0000

    added tests.

---
+++ pipeline/test_extract.py
@@ -0,0 +1,22 @@
+''''Tests extract.py functions.'''
+import pytest
+from unittest.mock import patch
+import requests_mock
+import requests
+from extract import get_top_stories, extract_story_info
+
+BASE_URL = "https://hacker-news.firebaseio.com/v0/"
+STORY_COUNT =200
+
+
+def test_get_stories_success(requests_mock):
+    """Test successful retrieval of stories from API."""
+
+    requests_mock.get(f"{BASE_URL}topstories.json",
+                        status_code=200, json=[{}])
+
+    get_top_stories(STORY_COUNT)
+
+    assert requests_mock.called
+    assert requests_mock.call_count == 1
+    assert requests_mock.last_request.method == "GET"
