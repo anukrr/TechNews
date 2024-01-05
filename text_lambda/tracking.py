e199ec66ca1f4bd6fcc355de6ca51e65d8de1eea
Jack Hayden <jackhaayden1@gmail.com>
2024-01-05 10:09:35 +0000

    Added viral function

--- text_lambda/tracking.py
+++ text_lambda/tracking.py
@@ -2,8 +2,14 @@
 from os import environ
 from dotenv import load_dotenv
 import psycopg2
+import pandas as pd
+
 
 load_dotenv()
+
+THRESHOLD = 20
+STORY_LIMIT = 200
+
 
 def get_db_connection():
     """Forms AWS RDS postgres connection."""
@@ -15,13 +21,13 @@ def get_db_connection():
             port=environ["DB_PORT"])
         return conn
     except Exception as e:
-        print(f'Error: Unable to form connection {e}')
+        print('Error: Unable to form connection %s', e)
 
 
-def viral_checker(threshold: int):
-    conn = get_db_connection()
-    with conn.cursor() as curr:
-        curr.execute(f"""
+def viral_checker(threshold: int, story_limit: int) -> list[dict]:
+    """Returns a list of dictionaries with each story above the score threshold."""
+
+    viral_query = """
         WITH LatestTwoRecords AS (
         SELECT
             story_id,
@@ -37,20 +43,27 @@ def viral_checker(threshold: int):
             LatestTwoRecords
         WHERE
             row_num <= 2
-            AND story_id IN (SELECT story_id FROM records ORDER BY record_id DESC LIMIT 200)
+            AND story_id IN (SELECT story_id FROM records ORDER BY record_id DESC LIMIT (%s))
         GROUP BY
             story_id
         HAVING
-            COALESCE(MAX(CASE WHEN row_num = 1 THEN score END) - MAX(CASE WHEN row_num = 2 THEN score END), 0) > {threshold}
+            COALESCE(MAX(CASE WHEN row_num = 1 THEN score END) - MAX(CASE WHEN row_num = 2 THEN score END), 0) > (%s)
         ORDER BY
             score_difference DESC;
-        """)
-        data = curr.fetchall()
+        """
 
-        curr.close()
-    print(data)
+    conn = get_db_connection()
+    with conn.cursor() as curr:
+        curr.execute(viral_query, (story_limit, threshold))
+        story_ids = tuple([story_id[0] for story_id in curr.fetchall()])
+        story_info_query = f"""SELECT title, story_url FROM stories WHERE story_id IN {story_ids};"""
+        curr.execute(story_info_query)
+        story_info = curr.fetchall()
+        story_info = pd.read_sql(story_info_query, conn)
+    story_info = story_info.to_dict(orient="records")
+    return story_info
 
 
 if __name__ == "__main__":
     
-    viral_checker(100)
+    viral_checker(THRESHOLD, STORY_LIMIT)
