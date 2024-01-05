a61fe1e6b5568852f878383058f8c8ced6259441
KevinChirayil <kevin.chirayil@gmail.com>
2024-01-05 15:20:48 +0000

    fixed error handling

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,7 +1,8 @@
 """This script extracts story information from the Hacker News API."""
+
+import logging
 import pandas as pd
 from requests import get, exceptions
-import logging
 from dotenv import load_dotenv
 
 BASE_URL = "https://hacker-news.firebaseio.com/v0/"
@@ -9,39 +10,40 @@ BASE_URL = "https://hacker-news.firebaseio.com/v0/"
 
 def get_top_stories(count: int) -> list:
     """Returns the ids of the top 200 stories on Hacker News."""
+    if not isinstance(count, int):
+        raise TypeError("Count must be an integer.")
+
     try:
         top_stories = get(BASE_URL + "topstories.json", timeout=100).json()
         return top_stories[:count]
-    except exceptions.RequestException as e:
-        logging.exception(f"Error getting api request {e}")
-        raise Exception
+    except (exceptions.RequestException, exceptions.HTTPError, ValueError) as error:
+        logging.exception("An error occurred: %s", error)
+        raise
 
 
 def extract_story_info(story_id: int) -> dict:
     """Finds the details of a given story on Hacker News based on the story id."""
     try:
         story_info = get(BASE_URL + "item/" + str(story_id) +
-                        ".json", timeout=100).json()
-        relevant_cols = ["id", "title", "by", "url",
-                        "time", "score", "descendants", "type"]
-        story_dict = {col: story_info.get(col) for col in relevant_cols}
-        return story_dict
-    except exceptions.RequestException as e:
-         logging.exception(f"Error extracting story information {e}")
-         raise Exception
+                         ".json", timeout=100).json()
+    except (exceptions.RequestException, exceptions.HTTPError, ValueError) as error:
+        logging.exception("An error occurred: %s", error)
+        raise
+
+    relevant_cols = ["id", "title", "by", "url",
+                     "time", "score", "descendants", "type"]
+    story_dict = {col: story_info.get(col) for col in relevant_cols}
+    return story_dict
 
 
 def generate_dataframe(row_count: int) -> None:
     """Collects information on chosen number of top stories and returns them in a dataframe."""
-    try:
-        logging.info("Extraction Started.")
-        story_ids = get_top_stories(row_count)
-        all_stories = [extract_story_info(id) for id in story_ids]
-        return pd.DataFrame(all_stories)
-    except Exception as e:
-        logging.exception(f"Error extracting stories from api: {e}")
-        raise Exception
-    
+    logging.info("Extraction Started.")
+
+    story_ids = get_top_stories(row_count)
+    all_stories = [extract_story_info(id) for id in story_ids]
+    return pd.DataFrame(all_stories)
+
 
 if __name__ == "__main__":
     load_dotenv()
@@ -49,4 +51,3 @@ if __name__ == "__main__":
 
     stories_df = generate_dataframe(STORY_COUNT)
     stories_df.to_csv("extracted_stories.csv", index=False)
-
