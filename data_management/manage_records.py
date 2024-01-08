2613f6d13c6cbdad499582ebfe794d338a164c68
Jack Hayden <jackhaayden1@gmail.com>
2024-01-08 17:37:15 +0000

    Refactoring to lambda function

--- data_management/manage_records.py
+++ data_management/manage_records.py
@@ -1,13 +1,15 @@
-"""Contains the functionality to archive data that was recorded over a week ago into an S3 bucket."""
-from os import environ, path, listdir, remove
+"""Contains the functionality to archive week-old records to s3."""
+from os import environ, path
+from datetime import datetime, timedelta
 import pandas as pd
 from dotenv import load_dotenv
 from boto3 import client
 from botocore.exceptions import ClientError
-from datetime import datetime, timedelta
-from sqlalchemy import create_engine, URL
+from sqlalchemy import create_engine, URL, text
 
 load_dotenv()
+
+WEEK_DAY_COUNT = 7
 
 
 engine_url_object = URL.create(
@@ -26,7 +28,7 @@ def upload_file(s3_client: client, file: str, bucket: str, object_name=None):
         object_name = path.basename(file)
     try:
         s3_client.upload_file(file, bucket, object_name)
-    except ClientError as e:
+    except ClientError:
         return False
     return True
 
@@ -36,40 +38,39 @@ def get_month_file(s3_client: client, date: datetime.date) -> pd.DataFrame:
     current_year = date.year
     current_month = date.month
     try:
-        response = s3_client.get_object(Bucket="c9-tech-news", Key=f"{current_year}/{current_month}.csv")
-    except ClientError as error:
-        return "No such folder"
-    return response
+        response = s3_client.get_object(Bucket="c9-tech-news",
+                                        Key=f"{current_year}/{current_month}.csv").get("Body")
+    except ClientError:
+        return pd.DataFrame
+    try:
+        monthly_df = pd.read_csv(response)
+        return monthly_df
+    except pd.errors.EmptyDataError:
+        return pd.DataFrame
 
 
-
-def data_manager() -> None:
-    """Transfers records older than a week to an s3 bucket."""
+def lambda_handler(event, context):
+    """Transfers records older than a week to an s3 bucket & removes them from RDS."""
+    today = datetime.today()
     engine = create_engine(engine_url_object)
-    date_cutoff = datetime.now() - timedelta(days=3)
-    old_records = pd.read_sql(f"SELECT * FROM records WHERE record_time < '{date_cutoff}'", engine)
+    date_cutoff = datetime.now() - timedelta(days=WEEK_DAY_COUNT)
+    old_records = pd.read_sql(f"SELECT * FROM records WHERE record_time < '{date_cutoff}';",
+                               engine)
     old_records.to_csv("test_records.csv", index=False)
-    
-    s3 = client("s3",
-                aws_access_key_id=environ["AWS_ACCESS_KEY_ID"],
-                aws_secret_access_key=environ["AWS_SECRET_ACCESS_KEY"])
-
-
-
-if __name__ == "__main__":
 
     s3 = client("s3",
                 aws_access_key_id=environ["AWS_ACCESS_KEY_ID"],
                 aws_secret_access_key=environ["AWS_SECRET_ACCESS_KEY"])
-    print(get_month_file(s3, datetime.now()))
-    # load_dotenv()
-    # conn = get_db_connection()
-    # s3 = client("s3",
-    # aws_access_key_id=environ["AWS_ACCESS_KEY_ID"],
-    # aws_secret_access_key=environ["AWS_SECRET_ACCESS_KEY"])
+    month_df = get_month_file(s3, today)
+    if month_df.empty is False:
+        month_df = pd.concat([month_df, old_records], ignore_index=True)
+    else:
+        month_df = old_records
 
-    # with conn:
-    #     current_date = datetime.now().strftime('%Y-%m-%d')
+    file_key = f"{today.year}/{today.month}.csv"
+    csv_body = month_df.to_csv(index=False)
+    s3.put_object(Body=csv_body, Bucket="c9-tech-news", Key=file_key)
 
-    #     reading_data = pd.read_sql('SELECT * FROM stories;', conn)
-    #     reading_data.to_csv(f"{current_date}_backup_stories.csv" ,index=False)
+    with engine.connect() as conn:
+        delete_query = text(f"DELETE FROM records WHERE record_time < '{date_cutoff}';")
+        conn.execute(delete_query)
