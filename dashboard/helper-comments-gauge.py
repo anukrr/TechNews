1a6a7d6e2de5d2cff167dfe88e0c85531f87d4b6
KevinChirayil <kevin.chirayil@gmail.com>
2024-01-09 20:56:02 +0000

    gauge helper complete

--- dashboard/helper-comments-gauge.py
+++ dashboard/helper-comments-gauge.py
@@ -8,43 +8,44 @@ import streamlit as st
 BASE_URL = "https://hacker-news.firebaseio.com/v0/item/"
 
 
-def get_parent_comment_ids(story: int) -> list:
+def get_parent_comment_ids(story_id: int) -> list:
     """
-    Returns the id of comments from given story on Hacker News.
+    Returns the id of parent comments for a given story.
+    Note: in this case the API endpoint considers parent comments as "kids" of a story.
     """
-    story_info = requests.get(BASE_URL + f"{story}.json", timeout=30).json()
-    comment_ids = story_info.get("kids")  # Warning: read
-    return comment_ids
+    story_info = requests.get(BASE_URL + f"{story_id}.json", timeout=30).json()
+    return story_info.get("kids")  # Warning: see docstring
 
 
 def get_comment_text(comment: int) -> str:
     """Returns the text from a given comment."""
     comment_info = requests.get(
         BASE_URL + f"{comment}.json", timeout=30).json()
-    comment = comment_info.get("text")
-    return comment
+    return comment_info.get("text")
 
 
-def get_comment_list(story: int):
-    comments = get_parent_comment_ids(story)
-    comment_list = [get_comment_text(comment) for comment in comments]
-    return comment_list
+def get_comment_list(story_id: int):
+    comments = get_parent_comment_ids(story_id)
+    return [get_comment_text(comment) for comment in comments]
 
 
 def get_story_sentiment(story_id: int):
     """Performs sentiment analysis for a given story."""
     comment_list = get_comment_list(story_id)
     sentiment_list = []
+
     for comment in comment_list:
         blob = TextBlob(str(comment))
         for sentence in blob.sentences:
             sentiment = sentence.sentiment.polarity
         sentiment_list.append({'comment': comment,
                                'sentiment': sentiment})
+
     sentiment_mean = []
     for i in range(0, 20):
         sentiment_mean.append(sentiment_list[i]['sentiment'])
     average = sum(sentiment_mean)/len(sentiment_mean)
+
     return average
 
 
@@ -68,12 +69,13 @@ def categorise_sentiment(sentiment_value: int):
     return {'colour': category_color, 'label': category_label}
 
 
-def generate_sentiment_gauge(value: int):
-    category = categorise_sentiment(value)
+def generate_sentiment_gauge(sentiment_value: int):
+    """"""
+    category = categorise_sentiment(sentiment_value)
 
     fig = go.Figure(go.Indicator(
         mode="gauge+number",
-        value=value,
+        value=sentiment_value,
         title={'text': f"Overall Sentiment: {category.get('label')}"},
         gauge={'axis': {'range': [-1, 1]},
                'bar': {'color': category.get('colour')},
@@ -86,9 +88,8 @@ def make_gauge_chart(input_story_id: int):
     '''Bar chart showing transaction count per truck.'''
 
     value = get_story_sentiment(input_story_id)
-    gauge_fig = generate_sentiment_gauge(value)
-    # st.plotly_chart(sentiment_fig)
-    return st.plotly_chart(gauge_fig)
+    fig = generate_sentiment_gauge(value)
+    return st.plotly_chart(fig)
 
 
 if __name__ == "__main__":
