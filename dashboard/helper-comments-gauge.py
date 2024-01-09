45fbf052a4d7c992a6b49d4b9cf929b182a04504
KevinChirayil <kevin.chirayil@gmail.com>
2024-01-09 18:50:27 +0000

    relocate sentiment functions

--- dashboard/helper-comments-gauge.py
+++ dashboard/helper-comments-gauge.py
@@ -1,6 +1,8 @@
 """Script containing helper functions for comment analysis sentiment gauge plot."""
 
 import requests
+from textblob import TextBlob
+import plotly.graph_objects as go
 
 BASE_URL = "https://hacker-news.firebaseio.com/v0/item/"
 
@@ -26,3 +28,49 @@ def get_comment_list(story: int):
     comments = get_parent_comment_ids(story)
     comment_list = [get_comment_text(comment) for comment in comments]
     return comment_list
+
+
+def get_story_sentiment(story_id: str):
+    """Performs sentiment analysis for a given story."""
+    comment_list = get_comment_list(int(story_id))
+    sentiment_list = []
+    for comment in comment_list:
+        blob = TextBlob(str(comment))
+        for sentence in blob.sentences:
+            sentiment = sentence.sentiment.polarity
+        sentiment_list.append({'comment': comment,
+                               'sentiment': sentiment})
+    sentiment_mean = []
+    for i in range(0, 20):
+        sentiment_mean.append(sentiment_list[i]['sentiment'])
+    average = sum(sentiment_mean)/len(sentiment_mean)
+    return average
+
+
+def generate_sentiment_gauge(value: int):
+    """ """
+    if -1 <= value < -0.6:
+        category_color = "#FF0000"
+        category_label = "Very Negative 😠"
+    elif -0.6 <= value < -0.3:
+        category_color = "#FFA500"
+        category_label = "Negative 🙁"
+    elif -0.3 <= value < 0.3:
+        category_color = "#FFFF00"
+        category_label = "Neutral 😐"
+    elif 0.3 <= value < 0.6:
+        category_color = "#8BFF00"
+        category_label = "Positive 🙂"
+    else:
+        category_color = "#00FF00"
+        category_label = "Very Positive 😄"
+
+    fig = go.Figure(go.Indicator(
+        mode="gauge+number",
+        value=value,
+        title={'text': f"Overall Sentiment: {category_label}"},
+        gauge={'axis': {'range': [-1, 1]},
+               'bar': {'color': category_color},
+               }))
+
+    return fig
