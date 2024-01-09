85a98f5f866fa69e0b9d776705c87fb76aac5818
anukrr <argkrr@gmail.com>
2024-01-09 16:52:08 +0000

    Refactored Dash

--- NLP/text_cycle_complete.py
+++ NLP/text_cycle_complete.py
@@ -4,6 +4,7 @@ import time
 import threading
 import streamlit as st
 from requests import get
+import pandas as pd
 
 BASE_URL = "https://hacker-news.firebaseio.com/v0/item/"
 CLEANR = re.compile('<.*?>')
@@ -37,33 +38,55 @@ def get_top_5_most_replied_parent_comments(story_id: int):
             number_of_children = len(kids_info)
 
         parent_comments_list.append(
-            {'title': comment_title, 'number_of_children': number_of_children})
+            {'title': comment_title, 'number_of_children': number_of_children}) # add user name as well
 
     sorted_list = sorted(parent_comments_list, key=lambda comment_dict: comment_dict.get(
-        'number_of_children', 0), reverse=True)
+        'number_of_children', 0), reverse=True) 
     return sorted_list[:5]
 
 
-def cycle_text(text_list, interval=4):
-    index = 0
-    box = st.empty()
-    while True:
-        box.write(f'{text_list[index]}')  # st.text(text_list[index])
-        time.sleep(interval)
-        index = (index + 1) % len(text_list)
+# def cycle_text(text_list, interval=4):
+#     index = 0
+#     box = st.empty()
+#     while True:
+#         box.write(f'{text_list[index]}')  # st.text(text_list[index])
+#         time.sleep(interval)
+#         index = (index + 1) % len(text_list)
 
-def get_string_list():
-    # add user too
-    return [f"{parent_comment.get('title')} \n\n [{parent_comment.get('number_of_children')} replies]" for parent_comment in top_5_comments]
+# def get_string_list():
+#     # add user too
+#     return [f"{parent_comment.get('title')} \n\n [{parent_comment.get('number_of_children')} replies]" for parent_comment in top_5_comments]
+
+def generate_comments_df(top_comments: list) -> pd.DataFrame:
+    """"""
+    data = get_top_5_most_replied_parent_comments(38865518)
+    df = pd.DataFrame(data)
+    columns = ['Comment', 'Replies'] # add user column
+    df.columns = columns
+    return df
 
 if __name__ == "__main__":
-    st.title("What people are saying / hot discussion points:")
+    st.subheader('URL NLP analysis', divider='rainbow') # need to error filter for URLs not found at hackernews
+    url = st.text_input('Enter a URL', 'url')
+    st.write('Article', url)
+
+    st.subheader('URL NLP analysis', divider='rainbow')
+    st.write("Chec out the top talking points for this story:")
 
     story_id = 38865518
     top_5_comments = get_top_5_most_replied_parent_comments(story_id)
-    text_list = get_string_list()
 
-    cycle_text(text_list)
+    
+    # text_list = get_string_list()
+    # cycle_text(text_list)
 
-    title = st.text_input('Movie title', 'Life of Brian')
-    st.write('The current movie title is', title)
+    df = generate_comments_df(story_id)
+    for index, row in df.iterrows():
+        with st.expander(f"{row['Comment'][0:50]} {index + 1} - Replies: {row['Replies']}"):
+            st.write(row['Comment'])
+        # align replies to right side
+        # add poster username too
+ 
+
+
+    
