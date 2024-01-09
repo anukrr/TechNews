fe7e47d685b49b6b3630c5fc39f47726d44e0f4b
KevinChirayil <kevin.chirayil@gmail.com>
2024-01-09 18:41:11 +0000

    function: extract comment text

--- dashboard/helper-comments-gauge.py
+++ dashboard/helper-comments-gauge.py
@@ -12,3 +12,11 @@ def get_parent_comment_ids(story: int) -> list:
     story_info = requests.get(BASE_URL + f"{story}.json", timeout=30).json()
     comment_ids = story_info.get("kids")  # Warning: read
     return comment_ids
+
+
+def get_comment_text(comment: int) -> str:
+    """Returns the text from a given comment."""
+    comment_info = requests.get(
+        BASE_URL + f"{comment}.json", timeout=30).json()
+    comment = comment_info.get("text")
+    return comment
