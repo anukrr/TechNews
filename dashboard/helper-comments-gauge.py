ed0b1113c7fc615cf54359bf4ae25fd3fa2bb7d1
KevinChirayil <kevin.chirayil@gmail.com>
2024-01-09 19:54:24 +0000

    extensive refactor/repurpose of sentiment functions

--- dashboard/helper-comments-gauge.py
+++ dashboard/helper-comments-gauge.py
@@ -48,30 +48,35 @@ def get_story_sentiment(story_id: int):
     return average
 
 
-def generate_sentiment_gauge(value: int):
-    """ """
-    if -1 <= value < -0.6:
+def categorise_sentiment(sentiment_value: int):
+    if -1 <= sentiment_value < -0.6:
         category_color = "#FF0000"
         category_label = "Very Negative 😠"
-    elif -0.6 <= value < -0.3:
+    elif -0.6 <= sentiment_value < -0.3:
         category_color = "#FFA500"
         category_label = "Negative 🙁"
-    elif -0.3 <= value < 0.3:
+    elif -0.3 <= sentiment_value < 0.3:
         category_color = "#FFFF00"
         category_label = "Neutral 😐"
-    elif 0.3 <= value < 0.6:
+    elif 0.3 <= sentiment_value < 0.6:
         category_color = "#8BFF00"
         category_label = "Positive 🙂"
     else:
         category_color = "#00FF00"
         category_label = "Very Positive 😄"
 
+    return {'colour': category_color, 'label': category_label}
+
+
+def generate_sentiment_gauge(value: int):
+    category = categorise_sentiment(value)
+
     fig = go.Figure(go.Indicator(
         mode="gauge+number",
         value=value,
-        title={'text': f"Overall Sentiment: {category_label}"},
+        title={'text': f"Overall Sentiment: {category.get('label')}"},
         gauge={'axis': {'range': [-1, 1]},
-               'bar': {'color': category_color},
+               'bar': {'color': category.get('colour')},
                }))
 
     return fig
