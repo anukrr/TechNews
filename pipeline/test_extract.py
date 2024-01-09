1382fe03fd93c652f0d6172669444ec4e66a0d51
anukrr <argkrr@gmail.com>
2024-01-09 07:34:44 +0000

    updated extract tests

---
+++ pipeline/test_extract.py
@@ -0,0 +1,51 @@
+''''Tests extract.py functions.'''
+import pytest
+from extract import get_top_stories, extract_story_info, generate_dataframe
+import requests
+import pandas
+
+BASE_URL = "https://hacker-news.firebaseio.com/v0/"
+STORY_COUNT = 200
+
+
+def test_get_story_string():
+    """Checks if invalid input type raises Error."""
+    with pytest.raises(TypeError):
+        get_top_stories('2')
+
+
+def test_get_story_empty():
+    """Tests exception raised when no input provided."""
+    with pytest.raises(TypeError):
+        get_top_stories()
+
+
+def test_get_info_string():
+    """Tests exception raised when empty string provided."""
+    with pytest.raises(AttributeError):
+        extract_story_info(' ')
+
+
+def test_get_info_empty():
+    """Tests exception raised when no input provided."""
+    with pytest.raises(TypeError):
+        extract_story_info()
+
+def test_df_generated():
+    """Tests dataframe generated when correct input provided."""
+    df = generate_dataframe(5)
+    print(type(df))
+    assert type(df) == pandas.core.frame.DataFrame
+
+
+def test_get_stories_success(requests_mock):
+    """Test successful retrieval of stories from API."""
+
+    requests_mock.get(f"{BASE_URL}item/5.json",
+                      status_code=200, json=[{}])
+
+    get_top_stories(STORY_COUNT)
+
+    assert requests_mock.called
+    assert requests_mock.call_count == 1
+    assert requests_mock.last_request.method == "GET"
